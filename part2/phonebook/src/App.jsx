3bfddbc652b8ca97e88f44f640d3db0b84f989e7
Abdul <140862207+abdul2811@users.noreply.github.com>
2023-11-24 17:50:12 -0500

    added db.json and added axios + useEffect in app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({handleChange}) => {
   return (
@@ -34,12 +35,7 @@ const Persons = ({filtered}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -67,6 +63,18 @@ const App = () => {
   const handleNumberChange = event => setNewNumber(event.target.value)
 
   const handleFilterChange = event => setNewFilter(event.target.value)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   return (
     <div>
