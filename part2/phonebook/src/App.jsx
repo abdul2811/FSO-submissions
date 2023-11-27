b8b0397e0884bee094780c0d1aaf75da9ccb1e5c
Abdul <140862207+abdul2811@users.noreply.github.com>
2023-11-26 20:20:29 -0500

    added a notification pop up

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
+import './index.css'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import phoneService from './services/phone'
+import Notification from './components/Notification'
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   const filteredPersons = persons.filter(person => person.name && person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
@@ -25,6 +28,22 @@ const App = () => {
         .update(foundPerson.id, personObject)
         .then(response => {
           setPersons(persons.map(person => person.id !== foundPerson.id ? person : response.data))
+          setNewName('')
+          setNewNumber('')
+          setMessage(
+            `Replaced ${personObject.name}`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+        .catch(error => {
+          setMessage(
+            `${personObject.name} has already been removed, refresh the page`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         })
       }
     }
@@ -35,6 +54,12 @@ const App = () => {
           setPersons(persons.concat(response.data))
           setNewName('')
           setNewNumber('')
+          setMessage(
+            `Added ${personObject.name}`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
       })
     }
   }
@@ -52,7 +77,13 @@ const App = () => {
     const shouldDelete = window.confirm(`Delete ${person}`);
     if (shouldDelete){
       phoneService.deletion(id).then(
-        setPersons(persons.filter(person => person.id !== id))
+        setPersons(persons.filter(person => person.id !== id)),
+        setMessage(
+          `Deleted ${person}`
+        ),
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       )
     }
   }
@@ -72,6 +103,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter handleChange={handleFilterChange} />
       <h3>add a new</h3>
       <PersonForm add={addName} nameVal={newName} numVal={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
