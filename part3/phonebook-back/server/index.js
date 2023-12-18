8d6a8ffa1a75fb719d8c8e8510188f9d60bb5041
Abdul <140862207+abdul2811@users.noreply.github.com>
2023-12-18 18:12:46 -0500

    changed port

--- part3/phonebook-back/server/index.js
+++ part3/phonebook-back/server/index.js
@@ -108,7 +108,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
